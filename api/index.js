9dc1880be0a176e99ab7147391ee6cb9df310226
Joshua Ravael <jravaellnew@gmail.com>
2024-04-27 21:25:19 +0700

    a

--- api/index.js
+++ api/index.js
@@ -1,19 +1,19 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import db from "./config/Database.js";
+import db from "../src/config/Database.js";
 
-import Users from "./models/UserModel.js";
-import Banners from "./models/BannerModel.js";
+import Users from "../src/models/UserModel.js";
+import Banners from "../src/models/BannerModel.js";
 
-import UserRouter from "./routers/UserRouter.js";
+import UserRouter from "../src/routers/UserRouter.js";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
-import BannerRouter from './routers/BannerRouter.js';
-import Posts from "./models/PostModel.js";
-import Comments from "./models/CommentModel.js";
-import Categories from "./models/CategoryModel.js";
-import PostRouter from './routers/PostRouter.js'
+import BannerRouter from '../src/routers/BannerRouter.js';
+import Posts from "../src/models/PostModel.js";
+import Comments from "../src/models/CommentModel.js";
+import Categories from "../src/models/CategoryModel.js";
+import PostRouter from '../src/routers/PostRouter.js'
 
 dotenv.config();
 
@@ -47,7 +47,7 @@ const app = express();
     app.use(BannerRouter);
     app.use(PostRouter);
 
-    app.listen(443, () => console.log("Server is online!"));
+    app.listen(5000, () => console.log("Server is online!"));
   } catch (error) {
     console.error(error);
   }
