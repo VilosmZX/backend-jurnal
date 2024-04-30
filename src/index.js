c7068a0a6f8f67f1c84fb132d18de6fec6450510
Joshua Ravael <jravaellnew@gmail.com>
2024-04-30 17:00:34 +0700

    a

--- src/index.js
+++ src/index.js
@@ -41,6 +41,17 @@ const app = express();
         preflightContinue: true
       })
     );
+    app.use(function(req, res, next) {
+      res.header('Access-Control-Allow-Origin', 'https://www.jurnalsmatcr.site');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, X-Api-Key');
+      res.header('Access-Control-Allow-Credentials', 'true');
+      if ('OPTIONS' === req.method) {
+        res.sendStatus(200);
+      }
+      else {
+        next();
+    }})
 
     // Routers
     app.get('/', (req, res) => res.send('JURNALISTIK WEB API'));
